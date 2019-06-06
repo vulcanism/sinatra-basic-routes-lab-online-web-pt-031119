4a1d027723e228f3234d2affacc7642b0fd7a39e
Savannah C <sgcorrell@gmail.com>
2019-06-06 16:56:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/name" do
+    "My name is Savannah"
+  end
+  
+  get "/hometown" do
+    "My hometown is Fishers"
+  end
+  
+  get "/favorite-song" do
+    "My favorite song is 'Come Fly With Me'"
+  end
+  
 end
